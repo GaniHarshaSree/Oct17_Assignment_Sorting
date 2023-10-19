0276e12a1a0a0a66dc3556d113afbdf748ee7fc0
GaniHarshaSree <ganiharshasree18@gmail.com>
2023-10-19 15:21:27 +0530

    Add project files.

---
+++ SortingDemo/BubbleSortDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SortingDemo
+{
+    internal class BubbleSortDemo
+    {
+        static void BubbbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static public void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 15, 25, 85, 5, 355, 45, 95 };
+            Console.WriteLine("Original Array is :");
+            PrintArray(arr);
+            Console.WriteLine("--------------------");
+            BubbbleSort(arr);
+            Console.WriteLine("Sorted Array is :");
+            PrintArray(arr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortingDemo/SelectionSortDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingDemo
+{
+    internal class SelectionSortDemo
+    {
+        static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minimun_index = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minimun_index])
+                    {
+                        minimun_index = j;
+                    }
+                }
+                int temp = arr[minimun_index];
+                arr[minimun_index] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 30, 40, 20, 90, 10, 50, 60, 70, 80, 100 };
+            Console.WriteLine("Original Array is :");
+            PrintArray(arr);
+            Console.WriteLine("--------------------");
+            SelectionSort(arr);
+            Console.WriteLine("Sorted Array is :");
+            PrintArray(arr);
+            Console.ReadLine();
+        }
+
+
+    }
+}
